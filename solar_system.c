3496e389a76f6917cd083040614bff95c0a06f45
Louis B <louis.barboutie@gmail.com>
2024-03-16 21:23:37 +0100

    clean up overall structure

--- solar_system.c
+++ solar_system.c
@@ -14,43 +14,126 @@
 
 #include <python3.10/Python.h>
 
-double endTime;
+double endTime; // needs to be global for the heartbeat function
+
 void heartbeat(struct reb_simulation *r){
-    if (r->steps_done % 5000 == 0){
-        printf("\rCurrent simulation completion: %f %%, simulation steps: %li", r->t / endTime * 100, r->steps_done);
+    /*
+        function to print out the completion of the simulation
+    */
+    if (r->steps_done % 1000 == 0){
+        printf("\rCompletion: %.1f %%, Steps: %li", r->t / endTime * 100, r->steps_done);
     }
 }
 
-float frand(float range){
-    float randf = rand() / (float) RAND_MAX; // random number between 0 and 1
-    randf = randf * range - range / 2;
-    return randf;
+void save_state(char *file_path, struct reb_simulation *r){
+    /*
+    Save the states of the simulation to a file in non-binary format
+    */
+    FILE *f;
+    double x, y, z, vx, vy, vz;
+    printf("%s %s\n", "Saving the simulation state to", file_path);
+    f = fopen(file_path, "w");
+    for (int i=0; i<r->N; i++){
+        x = r->particles[i].x;
+        y = r->particles[i].y;
+        z = r->particles[i].z;
+        vx = r->particles[i].vx;
+        vy = r->particles[i].vy;
+        vz = r->particles[i].vz;
+        fprintf(f, "%f, %f, %f, %f, %f, %f\n", x, y, z, vx, vy, vz);
+    }
+    fclose(f);
+}
+
+void file_copy(char *ref_file_path, char *new_file_path){
+    FILE *fptr_ref, *fptr_dest;
+    char temp_ch;
+    
+    // open files
+    fptr_ref = fopen(ref_file_path, "r");
+    fptr_dest = fopen(new_file_path, "w");
+
+    /*
+    if (fptr_ref == NULL || fptr_dest == NULL){
+        Proc_err(DEF_ERR_OPT, "error opening file %s or %s", ref_file_path, new_file_path);
+    }
+    */
+
+    temp_ch = fgetc(fptr_ref);     // this is the temporary char which gets filled as each new char is read, and read the first char of the reference file
+    while (temp_ch != EOF){        // loop until reaching End Of File
+        fputc(temp_ch, fptr_dest); // write the char into the destination file 
+        temp_ch = fgetc(fptr_ref); // get next char in the reference file
+    }
+
+    // close files
+    fclose(fptr_ref);
+    fclose(fptr_dest);
+    return;
 }
 
 int main(void){
-    FILE    *f = fopen("../spice-scripts/BarycenterStates.txt", "r");
-    char    *line_buf = NULL;  //this has to be intialised NULL: won't be allocated unless it is NULL
-    size_t   line_buf_size = 0; //this variable is written to if zero, otherwise read.
-    ssize_t  line_size; //"Signed Size_Type" : size of a buffer, but can be -1 if failed.
-    double   state[6];
-    double   masses[10]; // masses of the bodies 
+
+    // ================= //
+    // === VARIABLES === //
+    // ================= //
+
+    // File i/o
+    FILE    *f;
+    char    *line_buf = NULL;   // this has to be intialised NULL: won't be allocated unless it is NULL
+    size_t   line_buf_size = 0; // this variable is written to if zero, otherwise read.
+    ssize_t  line_size;         // "Signed Size_Type" : size of a buffer, but can be -1 if failed.
+    
+    // state variables
+    double   state[6];               // positions and velocities of the bodies
+    double   masses[10];             // masses of the bodies 
     double   m, x, y, z, vx, vy, vz; // placeholders for adding particles to the simulation
-    bool CgenAsteroids = false;
 
-    if (f == NULL){
-        printf("Error opening the file\n");
-        return EXIT_FAILURE;
-    }
+    // conversion factors
+    double m2au = 6.6846e-12;
+    double km2m = 1000;
 
-    if (remove("archives/archive.bin") != 0){
-       printf("warning: archive could not be removed\n");
-    }
+    // Archive paths
+    char workArchivePath[] = "archive.bin";
+    char saveArchivePath[256];
+
+
+
+    // ======================================== //
+    // === INITIALIZATION OF THE SIMULATION === //
+    // ======================================== //
+
+    printf("Initializing the simulation ...\n");
 
     // initialize the rebound simulation
     struct reb_simulation *r  = reb_simulation_create();
+    int nBodies = 0;
+    int nAsteroids = 10;
+    int years = 100000;
+    int nSnapshots = 10;
+    endTime = 86400. * 365 * years; // one hundred years in seconds
+    double saveInterval = endTime / nSnapshots;
+    sprintf(saveArchivePath, "archives/archive_%i_years_%i_particles.bin", years, nAsteroids);
 
-    // change value of G to SI units
-    r->G = 6.6743e-11;
+    // set up simulation parameters
+    r->G = 6.6743e-11;           // change value of G to SI units
+    r->dt         = 20 * 86400.; // 20 days or a quarter of Mercury's orbit
+    r->N_active   = 10;          // only the planets are not test particles
+    r->heartbeat  = heartbeat;
+    r->integrator = REB_INTEGRATOR_WHFAST;
+    r->exact_finish_time = 0;
+
+    // remove the last archive because rebound always appends snapshots
+    (remove(workArchivePath) == 0) ? 
+        printf("Archive successfully removed\n"):
+        printf("warning: archive could not be removed\n");
+
+
+
+    // ======================================= //
+    // === ADD PARTICLES TO THE SIMULATION === //
+    // ======================================= //
+
+    printf("Adding bodies to the simulation ...\n");
 
     // manually define masses (in kg) of the planets from https://ssd.jpl.nasa.gov/planets/phys_par.html
     masses[0] = 1.32712440041279419e20 / 6.67430e-11;
@@ -64,194 +147,128 @@ int main(void){
     masses[8] = 102.4092e24;
     masses[9] = 13029.0e18;
 
-    // Add planets to the simulation
-    double m2au = 6.6846e-12;
-    int count = 0;
-    while ((line_size = getline(&line_buf, &line_buf_size, f)) != -1){
+    // open file with barycenter states
+    if ((f = fopen("../spice-scripts/BarycenterStates.txt", "r")) == NULL){
+        printf("Error opening the barycenter states file\n");
+        return EXIT_FAILURE;
+    }
 
-        // read the file with barycenter states
+    // read the file with barycenter states
+    while ((line_size = getline(&line_buf, &line_buf_size, f)) != -1){
         sscanf(line_buf, "%lf %lf %lf %lf %lf %lf", &state[0], &state[1], &state[2], &state[3], &state[4], &state[5]);
         if( line_size <= 0 ){
             printf("Reached end of file, or otherwise failed to read. Returned: %i\n", (int)line_size);
-            break; //exit the loop
+            break; 
         }
 
         // add solar system planets
         // convert position and velocities from km/s to m/s
-        m = masses[count];
-        x = state[0] * 1000;
-        y = state[1] * 1000;
-        z = state[2] * 1000;
-        vx = state[3] * 1000;
-        vy = state[4] * 1000;
-        vz = state[5] * 1000;
-        // printf("r = %f AU\n", pow(pow(x,2) + pow(y,2) + pow(z,2),0.5) * m2au);
-        // printf("v = %f m/s\n\n", pow(pow(vx,2) + pow(vy,2) + pow(vz,2),0.5));
+        m  = masses[nBodies];
+        x  = state[0] * km2m;
+        y  = state[1] * km2m;
+        z  = state[2] * km2m;
+        vx = state[3] * km2m;
+        vy = state[4] * km2m;
+        vz = state[5] * km2m;
         reb_simulation_add_fmt(r, "m x y z vx vy vz", m, x, y, z, vx, vy, vz);
-        count++;
+        nBodies++;
     }
     fclose(f);
 
-    int nAsteroids = 10000;
-    if (CgenAsteroids == true){
-        // add asteroid belt
-        // min distance : 1.6 A.U. (after Mars Orbit)
-        // max distance: 4.7 A.U. (before Jupiter orbit)
-        double au2km      = 149597870.7;
-        double minDist    = 1.6 * au2km * 1000;
-        double maxDist    = 4.7 * au2km * 1000;
-        double avgDist    = (maxDist + minDist ) / 2;
-        double ranDist;
-
-        // put asteroids on uniform circular motion and then offset position and velocity 
-        // with random numbers to produce keplerian orbits
-        
-        // printf("error here 1?\n");
-        // double pi = atan(1) * 4;
-        // printf("error here 2\n?");
-
-        printf("Computing circular motion parameters ... ");
-        #define PI 3.1415926
-        double dtheta = 2 * PI / nAsteroids;
-        double vCircular(double x, double y, double z){
-            return sqrt((r->G) * masses[0] / pow(pow(x,2) + pow(y,2) + pow(z,2),0.5));
-        }
-        printf("Done!\n");
-
-        // initialize random number generator
-        int seed = 42;
-        srand(seed);
-        double randRange = maxDist - minDist;
-        
-        printf("Adding asteroids to the simulation ... ");
-        for (int i=0; i<nAsteroids; i++){
-            double angle = i * dtheta;
-            ranDist = frand(randRange);
-            x = (avgDist + ranDist) * cos(angle);
-            y = (avgDist + ranDist) * sin(angle);
-            z = 0; // frand(randRange);
-            vx = - vCircular(x,y,z) * sin(angle); // - (v + frand(v/20)) * sin(angle);
-            vy =   vCircular(x,y,z)* cos(angle); //  (v + frand(v/20)) * cos(angle);
-            vz =   0; // frand(v/20);
-            // printf("r = %f AU\n", pow(pow(x,2) + pow(y,2) + pow(z,2), 0.5) * m2au);
-            // printf("v = %f\n\n", vCircular(x,y,z));
-            reb_simulation_add_fmt(r, "x y z vx vy vz", x, y, z, vx, vy, vz);
-        }
-        printf("Done!\n");
-    }
+    printf("Adding asteroids to simulation ...\n");
     
-    else {
-        printf("Calculating asteroid states ... ");
-        char cmdStr1[] = "python3 randAstOrbitGen.py "; 
-        char cmdStr2[64];
-        sprintf(cmdStr2, "%d", nAsteroids);
-        strcat(cmdStr1, cmdStr2);
-        system(cmdStr1);
-        printf("Done!\n");
-        printf("Adding asteroids to simulation ... ");
+    // Use python to generate the asteroid states
+    char cmdStr1[] = "python3 randAstOrbitGen.py "; 
+    char cmdStr2[64];
+    sprintf(cmdStr2, "%d", nAsteroids);
+    strcat(cmdStr1, cmdStr2);
+    system(cmdStr1);
 
-        f = fopen("asteroidStates.csv", "r");
-
-        if (f == NULL){
-            printf("\nError opening the file\n");
-            return EXIT_FAILURE;
-        }
-
-        while ((line_size = getline(&line_buf, &line_buf_size, f)) != -1){
-
-            // read the file with asteroid states
-            sscanf(line_buf, "%lf %lf %lf %lf %lf %lf", &state[0], &state[1], &state[2], &state[3], &state[4], &state[5]);
-            if( line_size <= 0 ){
-                printf("Reached end of file, or otherwise failed to read. Returned: %i\n", (int)line_size);
-                break; //exit the loop
-            }
-
-            // add asteroids
-            x = state[0];
-            y = state[1];
-            z = state[2];
-            vx = state[3];
-            vy = state[4];
-            vz = state[5];
-            // printf("r = %f AU\n", pow(pow(x,2) + pow(y,2) + pow(z,2),0.5) * m2au);
-            // printf("v = %f m/s\n\n", pow(pow(vx,2) + pow(vy,2) + pow(vz,2),0.5));
-            reb_simulation_add_fmt(r, "x y z vx vy vz", x, y, z, vx, vy, vz);
-            count++;
-        }
-
-        fclose(f);
-        printf("Done!\n");
+    // Open the file with the asteroid states
+    if ((f = fopen("asteroidStates.csv", "r")) == NULL){
+        printf("Error opening the asteroid states file\n");
+        return EXIT_FAILURE;
     }
-    
 
-    printf("Setting up the simulation parameters ... ");
+    // read the file line by line 
+    while ((line_size = getline(&line_buf, &line_buf_size, f)) != -1){
+        sscanf(line_buf, "%lf %lf %lf %lf %lf %lf", &state[0], &state[1], &state[2], &state[3], &state[4], &state[5]);
+        if( line_size <= 0 ){
+            printf("Reached end of file, or otherwise failed to read. Returned: %i\n", (int)line_size);
+            break;
+        }
+
+        // add asteroids
+        x  = state[0];
+        y  = state[1];
+        z  = state[2];
+        vx = state[3];
+        vy = state[4];
+        vz = state[5];
+        reb_simulation_add_fmt(r, "x y z vx vy vz", x, y, z, vx, vy, vz);
+    }
+    fclose(f);
+    
+    // move the origin to center of mass so that the simulation doesn't drift away
     reb_simulation_move_to_com(r);
-    // do funky simulation
-    int years = 100000;
-    endTime = 86400. * 365 * years; // * 1000; // one hundred years in seconds
-    int nSnapshots = 10;
-    double saveInterval = endTime / nSnapshots;
-    char archivePath[256];
-    sprintf(archivePath, "archives/archive_%i_years_%i_particles.bin", years, nAsteroids);
-    reb_simulation_save_to_file_interval(r, archivePath, saveInterval);
-    r->dt         = 8640000.;
-    r->N_active   = 10; // only the planets are not test particles
-    r->heartbeat  = heartbeat;
-    r->integrator = REB_INTEGRATOR_WHFAST;
-    printf("Done!\n");
+    
+    // Save the initial conditions to csv file
+    save_state("input.csv", r);
 
-    printf("Saving the input state ... ");
 
-    FILE *fInput;
 
-    fInput = fopen("input.csv", "w");
-    for (int i=0; i<r->N_active + nAsteroids; i++){
-        x = r->particles[i].x;
-        y = r->particles[i].y;
-        z = r->particles[i].z;
-        vx = r->particles[i].vx;
-        vy = r->particles[i].vy;
-        vz = r->particles[i].vz;
-        fprintf(fInput, "%f, %f, %f, %f, %f, %f\n", x, y, z, vx, vy, vz);
-    }
-
-    fclose(fInput);
-
-    printf("Done!\n");
+    // ============================== //
+    // === RUNNING THE SIMULATION === //
+    // ============================== //
 
     printf("Running the simulation ...\n");
-    // int nSimulationSteps = 10000;
-    // reb_simulation_steps(r, nSimulationSteps);
-    reb_simulation_integrate(r, endTime);
-    printf("Done!\n");
-
-    /*
-        Print positions of asteroids to csv file
-    */
-
-   printf("Saving output state ... ");
     
-    FILE *fOutput = fopen("output.csv", "w");
-    
-    for (int i = 0; i < r->N_active + nAsteroids; i++){
-        x = r->particles[i].x;
-        y = r->particles[i].y;
-        z = r->particles[i].z;
-        vx = r->particles[i].vx;
-        vy = r->particles[i].vy;
-        vz = r->particles[i].vz;
-        fprintf(fOutput, "%f, %f, %f, %f, %f, %f\n", x, y, z, vx, vy, vz);
+    // integrate with snapshot saving 
+    // reb_simulation_save_to_file_interval(r, archivePath, 365 * 86400.); // use this if using time
+    printf("Saving the initial snapshots ...\n");
+    for (int i=0; i<10; i++){
+        reb_simulation_save_to_file(r, workArchivePath);
+        reb_simulation_step(r);
     }
 
-    fclose(fOutput);
+    // reb_simulation_save_to_file_step(r, workArchivePath, 4);
+    // reb_simulation_steps(r, 4 * nSnapshots);
     
-    printf("Done!\n");
+    // reb_simulation_free(r);
+
+    // reload the last state from the archive to continue integration without snapshots
+    printf("Time travelling to the future ...\n");
+    // struct reb_simulationarchive* archive = reb_simulationarchive_create_from_file(workArchivePath);
+    // reb_simulationarchive_free(archive);
+    // reb_simulation_steps(r, 100000);
+    reb_simulation_integrate(r, endTime);
+
+    // save more snapshots of the end of the simulation
+    printf("Saving the final snapshots ...\n");
+    for (int i=0; i<10; i++){
+        reb_simulation_save_to_file(r, workArchivePath);
+        reb_simulation_step(r);
+    }
+
+    printf("\n");
+    
+
+
+    // ========================================= //
+    // === POST PROCESSING OF THE SIMULATION === //
+    // ========================================= //
+
+    // Save the final conditions to csv file
+    save_state("output.csv", r);
+   
+    // run the plotting script
+    system("python3 kirkwood.py");
 
     // free the memory of the simulation after being done
     reb_simulation_free(r);
 
-    // run the plotting script
-    system("python3 kirkwood.py");
+    // archive the simulation
+    file_copy(workArchivePath, saveArchivePath);
 
     return EXIT_SUCCESS;
 }
